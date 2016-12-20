1c21380e0c77ada0b12a6aff3f7a22fdcc844fb6
Alana Smith <alana.rae.smith@gmail.com>
2016-12-20 17:05:24 -0600

    Add files via upload
    Completed JavaScript exercise 3

---
+++ boybands.js
@@ -0,0 +1,35 @@
+
+// Loop through the two arrays provided (bands and vegetables) and output each element
+// in the arrays into their corresponding HTML <div> element. Ensure that each item is
+// in a block element (e.g. li, div, p. etc...)
+
+var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+
+// The number of loops to perform (what if the array changes?)
+var loopCount = bands.length;
+
+// Get a reference to the appropriate DOM element for bands
+var bandElement = document.getElementById("boy-bands");
+
+// Get a reference to the appropriate DOM element for vegetables
+var veggieElement = document.getElementById("vegetables");
+
+
+
+// Start looping
+for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+
+// Keep track of which band we're on in the loop
+var currentBand = bands[loopTracker];
+
+// Keep track of which veggie we're on in the loop
+var currentVeggie = vegetables[loopTracker];
+
+// Add the band names into the correct <div>
+bandElement.innerHTML += '<p>' + currentBand + '</p>';
+
+// Add the veggie names into the correct <div>
+veggieElement.innerHTML += '<p>' + currentVeggie + '</p>';
+
+}
